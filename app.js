50eb6e7d0cfa14081d420ef5f5b707d87c0128fe
erwinlobos <elobos@tigo.com.gt>
2017-06-02 11:56:19 -0600

    Correccion de sensacion termica

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/clima/:ciudad?', function (req, res) {
             latitud : results.latitude,
             longitud : results.longitude,
             temperatura : weatherResults.temperature,
-            sensacionTermina : weatherResults.apparentTemperature
+            sensacionTermica : weatherResults.apparentTemperature
           }          
           res.send(resp)
         }
