5063fd832478eacf806b6c35e855baa8680f3b1f
erwinlobos <elobos@tigo.com.gt>
2017-06-02 08:45:33 -0600

    Cambio de puerto

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.get('/clima/:ciudad?', function (req, res) {
   });
 })
 
-app.listen(80, function() {
-  console.log('Node app is running on port 80');
+app.listen(8080, function() {
+  console.log('Node app is running on port 8080');
 });
